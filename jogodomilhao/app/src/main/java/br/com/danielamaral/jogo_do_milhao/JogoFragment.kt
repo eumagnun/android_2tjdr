62364dfe32132984c2a970480754a037137f4bc2
Daniel Amaral <eumagnun@gmail.com>
2022-05-05 22:42:10 -0300

    versao inicial jogo-do-milhao

---
+++ jogodomilhao/app/src/main/java/br/com/danielamaral/jogo_do_milhao/JogoFragment.kt
@@ -0,0 +1,27 @@
+package br.com.danielamaral.jogo_do_milhao
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import br.com.danielamaral.jogo_do_milhao.databinding.FragmentJogoBinding
+import br.com.danielamaral.show_do_milhao.Database
+
+class JogoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding:FragmentJogoBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_jogo,container,false)
+
+        binding.tvPergunta.text = Database.perguntas[0].texto
+        binding.rbResposta1.text = Database.perguntas[0].respostas[0].texto
+        binding.rbResposta2.text = Database.perguntas[0].respostas[1].texto
+        binding.rbResposta3.text = Database.perguntas[0].respostas[2].texto
+
+        return binding.root
+    }
+}
---
+++ jogodomilhao/app/src/main/java/br/com/danielamaral/jogo_do_milhao/model/Resposta.kt
@@ -0,0 +1,5 @@
+package br.com.danielamaral.jogo_do_milhao.model
+
+data class Resposta
+    (val texto: String,
+     val correta: Boolean)
